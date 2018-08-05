208240a3c87d91c9e79cd1bdd61e39ea5b4a4040
Dan Chung <danetix@gmail.com>
2018-08-05 14:49:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = 'Dan'
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
